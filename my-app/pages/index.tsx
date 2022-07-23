70d7c0edc7fcb369aeb4e9605f3dfa27109ea4c8
alpaca0984 <alpaca0984@gmail>
2022-07-23 12:05:53 +0200

    fix(my-app): download svg and fix CSS

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -402,7 +402,7 @@ const Home: NextPage = () => {
           )}
         </div>
         <div>
-          <img className={styles.image} src="./0.svg" />
+          <img className={styles.image} src="./cryptodevs/0.svg" />
         </div>
       </div>
 
